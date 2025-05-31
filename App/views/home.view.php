43ff7dd57592edd74398d4ce39047aab68991e7a
RautuGabrielCosmin <rautu_cosmin21@yahoo.com>
2025-05-31 12:54:35 +0300

    stable version

--- App/views/home.view.php
+++ App/views/home.view.php
@@ -29,6 +29,7 @@
                                 <strong>Tags:</strong> <?= $listing->tags ?>
                             </li>
                         </ul>
+                        <!-- listing/id inseatd of listing?id= -->
                         <a href="/listing/<?= $listing->id ?>"
                             class="block w-full text-center px-5 py-2.5 shadow-sm rounded border text-base font-medium text-indigo-700 bg-indigo-100 hover:bg-indigo-200">
                             Details
@@ -42,7 +43,6 @@
             Show All Jobs
         </a>
 </section>
-
 
 <?php loadPartial("bottom-banner") ?>
 <?php loadPartial("footer-banner") ?>
